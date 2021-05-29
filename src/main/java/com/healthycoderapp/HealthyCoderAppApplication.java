716dec8c5e661a83170fcef8138191083f74d22c
MargulanAubakirov <nalugram@gmail.com>
2021-05-29 12:52:02 -0500

    Initial commit

---
+++ src/main/java/com/healthycoderapp/HealthyCoderAppApplication.java
@@ -0,0 +1,13 @@
+package com.healthycoderapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthyCoderAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HealthyCoderAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/healthycoderapp/HealthyCoderAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.healthycoderapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthyCoderAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
